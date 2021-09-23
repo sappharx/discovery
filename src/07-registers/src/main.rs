67324330f1b7051abdb3dea1010b6a1e58b26edd
Vincent Sisk <vincent.sisk@workday.com>
2021-09-23 17:18:40 -0600

    hack 07

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,29 +1,59 @@
 #![no_main]
 #![no_std]
 
+// use core::ptr;
+
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprint, iprintln, ITM, RegisterBlock};
+
+// Print the current contents of odr
+// fn iprint_odr(itm: &mut ITM) {
+//     const GPIOE_ODR: u32 = 0x4800_1014;
+
+//     unsafe {
+//         iprintln!(
+//             &mut itm.stim[0],
+//             "ODR = 0x{:04x}",
+//             ptr::read_volatile(GPIOE_ODR as *const u16)
+//         );
+//     }
+// }
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    // aux7::init();
+    // let mut itm = aux7::init().0;
+    let gpioe = aux7::init().1;
 
-    unsafe {
+    // unsafe {
+        // ptr::read_volatile(0x4800_1800 as *const u32);
+
         // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+        // const GPIOE_BSRR: u32 = 0x48001018;
+
+        // Print the initial contents of ODR
+        // iprint_odr(&mut itm);
 
         // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
+        gpioe.bsrr.write(|w| w.bs9().set_bit());
+        // ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+        // iprint_odr(&mut itm);
 
         // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
+        gpioe.bsrr.write(|w| w.bs11().set_bit());
+        // ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+        // iprint_odr(&mut itm);
 
         // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+        gpioe.bsrr.write(|w| w.br9().set_bit());
+        // ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+        // iprint_odr(&mut itm);
 
         // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
-    }
+        gpioe.bsrr.write(|w| w.br11().set_bit());
+        // ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+        // iprint_odr(&mut itm);
+    // }
 
     loop {}
 }
